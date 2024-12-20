ee93f4df51a03d5e7d90b7ff7f79d644cb5cbc95
Olusoji <esohjay3@gmail.com>
2024-12-20 04:50:06 +0000

    adjust width

--- src/screens/Template.tsx
+++ src/screens/Template.tsx
@@ -8,12 +8,16 @@ function Template() {
     <main className="flex justify-center bg-primaryLight">
       {/* <MobileNav />
        */}
-      <section className={`bg-white w-full `}>
-        <TopNav />
-        <DesktopNav />
-        <section className="lg:ml-80 px-5 lg:pr-20 mt-[72px] lg:mt-[88px] mb-[95px] lg:mb-[10px] ">
-          <Outlet />
-          <MobileNav />
+      <section
+        className={`bg-white w-full max-w-screen-lg flex justify-center items-center`}
+      >
+        <section className="w-full">
+          <TopNav />
+          <DesktopNav />
+          <section className="lg:ml-80 px-5 lg:pr-20 mt-[72px] lg:mt-[88px] mb-[95px] lg:mb-[10px] ">
+            <Outlet />
+            <MobileNav />
+          </section>
         </section>
       </section>
     </main>
